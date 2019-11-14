772f1355167ebd33ef99be7a0dbbec7e7f355ed1
Qin <mm739023340@qq.com>
2019-11-14 22:43:17 +0800

    完成文件系统资源管理器加载

--- GameEngine3dV2/src/main/java/com/lisp/engine/util/RenderUtil.java
+++ GameEngine3dV2/src/main/java/com/lisp/engine/util/RenderUtil.java
@@ -9,12 +9,17 @@ import static org.lwjgl.opengl.GL30.GL_FRAMEBUFFER_SRGB;
 *  description:渲染工具 完成清屏初始化的工作
 */
 public class RenderUtil {
+    /*
+    * 清屏
+    * */
     public static void clearScreen()
     {
         //TODO: Stencil Buffer
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     }
-
+    /*
+    * 初始化界面
+    * */
     public static void initGraphics()
     {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -27,5 +32,12 @@ public class RenderUtil {
         //TODO: Depth clamp for later
 
         glEnable(GL_FRAMEBUFFER_SRGB);
+    }
+    /*
+    * 打印版本号
+    * */
+    public static String getOpenGLVersion()
+    {
+        return glGetString(GL_VERSION);
     }
 }
